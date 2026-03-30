ab616b3e888aee7456846839171fe28449b6e7ca
unknown <irfanchaneliparambil@gmail.com>
2026-03-30 15:48:08 +0530

    fix bcrypt error

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const passport = require('passport');
+// const passport = require('passport');
 
 const router = express.Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect(`http://localhost:4000/auth/success?token=${req.user.token}&name=${req.user.user.name}`);
-});
+// router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+//     res.redirect(`http://localhost:4000/auth/success?token=${req.user.token}&name=${req.user.user.name}`);
+// });
 
 router.get('/logout', (req, res) => {
     req.logout();
